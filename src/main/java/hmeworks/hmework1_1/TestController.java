8276979e34f1f6e17b7e5a48e2898a8c7bb79690
HomeWorks <evk.05@mail.ru>
2024-06-13 14:10:52 +0300

    init project 1.1

---
+++ src/main/java/hmeworks/hmework1_1/TestController.java
@@ -0,0 +1,11 @@
+package hmeworks.hmework1_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "hello";
+    }
+ }
